6da1993fbc0976c5a7fe145055cd918f31048aa5
EruslanovAndrei <eruslanov84as@gmail.com>
2022-11-04 17:48:15 +0300

    start write programm

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите кол-во элементов: ");
+string[] s = new string[10];
+int n = int.Parse(Console.ReadLine());;
+
+
+
+
+for (int i = 0; i < n; i++)
+{
+    s[i] = Console.ReadLine();
+}
+for (int i = 0; i < n; i++)
+{
+    Console.Write(s[i] + ",");
+}
+
+
