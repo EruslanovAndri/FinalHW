68fac56eaec74376f0a432c1cfc3a5c575aa5b23
EruslanovAndrei <eruslanov84as@gmail.com>
2022-11-05 15:05:51 +0300

    add empty bs.drawio.png file

--- Program.cs
+++ Program.cs
@@ -1,25 +1,35 @@
 ﻿Console.Clear();
 Console.Write("Введите кол-во элементов: ");
-string[] s = new string[20];
-int n = int.Parse(Console.ReadLine()); ;
+string[] arrayOne = new string[20];
+int n = int.Parse(Console.ReadLine());
 
 
-void PrintStringArray(int n)
+
+
+void FillAndPrintStringArray(int n)
 {
     for (int i = 0; i < n; i++)
     {
-        s[i] = Console.ReadLine();
+        arrayOne[i] = Console.ReadLine();
     }
     for (int i = 0; i < n; i++)
     {
         if (i == 0) Console.Write("[");
-        if (i < n - 1) Console.Write(s[i] + ",");
-        else Console.WriteLine(s[i] + "]");
+        if (i < n - 1) Console.Write(arrayOne[i] + ",");
+        else Console.WriteLine(arrayOne[i] + "]");
     }
-
 }
 
-PrintStringArray(n);
+
+FillAndPrintStringArray(n);
+
+
+
+
+
+
+
+
 
 
 
